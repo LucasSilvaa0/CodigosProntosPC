da108ec76c4cf832a126dc5de2266f0aff719ebc
Lucas da Silva Carvalho <lsc8@windows.cin.ufpe.br>
2025-10-02 15:25:05 -0300

    Código de início

---
+++ startCode.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    return 0;
+}
