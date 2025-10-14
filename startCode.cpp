087046956544931cede94483c241d475a407e721
LucasSilvaa0 <lucaspernambucano@live.com>
2025-10-13 22:30:49 -0300

    Atualizando código pronto para questões

--- startCode.cpp
+++ startCode.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <algorithm>
+#include <math.h>
+#include <sstream>
+#include <stack>
+#include <map>
 #include <vector>
 #include <set>
-#include <map>
 #include <queue>
+#include <string>
 using namespace std;
+#define ll long long
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
 
     return 0;
 }
